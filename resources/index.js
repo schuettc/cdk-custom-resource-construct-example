5643f78fd47719fb2df32965fd4bdd15947ce807
Court Schuett <schuettc@amazon.com>
2021-12-30 12:40:01 -0600

    2nd

--- resources/index.js
+++ resources/index.js
@@ -1,36 +1,33 @@
 exports.handler = function (event, context) {
   console.log('REQUEST RECEIVED:\n' + JSON.stringify(event));
 
+  const responseData = {
+    Status: '',
+    Reason: '',
+    LogicalResourceId: event.LogicalResourceId,
+    PhysicalResourceId: 'CustomResourceExample',
+    RequestId: event.RequestId,
+    StackId: event.StackId,
+  };
+
   if (event.RequestType == 'Delete') {
-    return {
-      Status: 'SUCCESS',
-      Reason: 'Nothing to do',
-      LogicalResourceId: event.LogicalResourceId,
-      PhysicalResourceId: 'CustomResourceExample',
-      RequestId: event.RequestId,
-      StackId: event.StackId,
-    };
+    responseData.Status = 'SUCCESS';
+    responseData.Reason = 'Nothing to do.';
+    console.log({ responseData });
+    return responseData;
   }
 
   try {
-    responseData['Result'] = event.ResourceProperties.CustomResourceNumber * 2;
-    return {
-      Status: 'SUCCESS',
-      Reason: 'Mulitplied',
-      LogicalResourceId: event.LogicalResourceId,
-      PhysicalResourceId: 'CustomResourceExample',
-      RequestId: event.RequestId,
-      StackId: event.StackId,
-      Data: responseData,
-    };
+    const multiplyResult = event.ResourceProperties.CustomResourceNumber * 2;
+    responseData.Status = 'SUCCESS';
+    responseData.Reason = 'Successfully multipled';
+    responseData['Data']['Result'] = multiplyResult;
+    console.log({ responseData });
+    return responseData;
   } catch (error) {
-    return {
-      Status: 'FAILURE',
-      Reason: JSON.stringify(error),
-      LogicalResourceId: event.LogicalResourceId,
-      PhysicalResourceId: 'CustomResourceExample',
-      RequestId: event.RequestId,
-      StackId: event.StackId,
-    };
+    responseData.Status = 'FAILURE';
+    responseData.Reason = JSON.stringify(error);
+    console.log({ responseData });
+    return responseData;
   }
 };
