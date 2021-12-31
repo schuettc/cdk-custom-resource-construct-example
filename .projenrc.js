c2148867c742b0a7187cab2bcce28c6f6825a276
Court Schuett <schuettc@amazon.com>
2021-12-30 21:31:22 -0600

    release

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,5 @@
 const { awscdk } = require('projen');
+
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Court Schuett',
   authorAddress: 'schuettc@amazon.com',
@@ -6,7 +7,13 @@ const project = new awscdk.AwsCdkConstructLibrary({
   defaultReleaseBranch: 'main',
   peerDependencies: ['aws-cdk-lib'],
   name: 'cdk-custom-resource-construct-example',
+  license: 'MIT-0',
   npmignoreEnabled: true,
+  autoApproveOptions: {
+    allowedUsernames: ['schuettc'],
+    secret: 'GITHUB_TOKEN',
+  },
+  autoApproveUpgrades: true,
   repositoryUrl:
     'https://github.com/schuettc/cdk-custom-resource-construct-example.git',
 });
